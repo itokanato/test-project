265636bc507d884cb1e6bc5b19455190092a7271
itokanato <79110kanato@gmail.com>
2025-04-10 02:10:16 +0900

    feature ダッシュボード画面の簡単なカスタマイズ

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,24 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    {{ Auth::user()->name }}さん、ようこそ
+                    <br>
                     {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <x-primary-button>
+                        ボタン
+                    </x-primary-button>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </x-app-layout>
