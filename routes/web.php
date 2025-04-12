1b23764d712f9596dea93355e23a31c6e6cc30b2
itokanato <79110kanato@gmail.com>
2025-04-12 15:49:17 +0900

    refactor:ルート設定のコメント修正

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,17 @@ Route::middleware('auth')->group(function () {
 });
 
 // 投稿関連
-// 一覧画面
+// 一覧表示
 Route::get('/post', [PostController::class, 'index'])->name('post.index');
-// 個別表示画面
+// 個別表示
 Route::get('post/show/{post}', [PostController::class, 'show'])->name('post.show');
-// フォーム画面
+// 作成フォーム表示
 Route::get('/post/create', [PostController::class, 'create']);
-// 投稿
+// 保存
 Route::post('/post', [PostController::class, 'store'])->name('post.store');
-// 編集画面
+// 編集フォーム表示
 Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-// 編集
+// 更新
 Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
 // 削除
 Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
