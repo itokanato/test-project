3ebd6b77b309b9c3366344b9017f36a29203e98b
itokanato <79110kanato@gmail.com>
2025-04-13 01:15:45 +0900

    feature:テストデータの作成
    - シーダーを使用したテストデータの作成
    - ファクトリーを使用したテストデータの作成
    - WHY:Postモデルにuse HasFactoryと記述することでPostFactoryを使用してシーダーを実行できる。これがないとPostFactoryを使用してシーダーを実行するときにエラーが発生する。

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    /* WHY:use HasFactoryと記述することでPostFactoryを使用してシーダーを実行できる。
+     *     これがないとPostFactoryを使用してシーダーを実行するときにエラーが発生する。
+     */
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'body',
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    use HasFactory;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(20),
+            'body' => fake()->text(50),
+            'user_id' => \App\Models\User::factory(),
+        ];
+    }
+}
